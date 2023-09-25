99f94868f7d36f86e027d2eb630c19221dc05350
Rudson R Alves <rudsonalves@yahoo.com.br>
2023-09-25 08:12:48 -0300

    ...

--- lib/features/home_page/home_page.dart
+++ lib/features/home_page/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../common/models/user_name_notifier.dart';
