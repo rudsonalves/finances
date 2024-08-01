a900b853f0424c54691ea3c1099a3c44ae5ce0ef
rudsonalves <rudsonalves67@gmail.com>
2024-08-01 18:03:30 -0300

    Adjustments for compiling on API 34

--- lib/common/constants/app_info.dart
+++ lib/common/constants/app_info.dart
@@ -1,20 +1,3 @@
-// Copyright (C) 2024 rudson
-//
-// This file is part of finances.
-//
-// finances is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// finances is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with finances.  If not, see <https://www.gnu.org/licenses/>.
-
 import 'dart:developer';
 
 import 'package:flutter/services.dart';
@@ -24,14 +7,13 @@ class AppInfo {
   AppInfo._();
 
   static const name = 'finances';
-  static const description = 'financial control app';
-  static const version = '1.1.01+89';
+  static const description = 'financial control app licensed under the GPLv3';
+  static const version = '1.1.03+91';
 
   static get pageUrl => 'https://rralves.dev.br/en/$name/';
   static const email = 'alvesdev67@gmail.com';
-  static const privacyPolicyUrl =
-      'https://rralves.dev.br/en/privacy-policy-en/';
-
+  static const privacyPolicyUrl = 'https://rralves.dev.br/en/privacy-policy-en/';
+  
   static Future<void> launchUrl(String url) async {
     final uri = Uri.parse(url);
 
